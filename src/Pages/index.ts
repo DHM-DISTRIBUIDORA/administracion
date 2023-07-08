f5175ce63d9bf0a6fa381396552ae3571e25b2e6
Licethd <liceth.dfe@gmail.com>
2023-07-08 00:53:57 -0400

    dm_usuarios

--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -2,7 +2,6 @@ import { SPage, SPageListProps } from 'servisofts-component';
 
 import root from './root';
 import carga from "./carga"
-// import mapa from './mapa';
 import login from "./login";
 import registro from './registro';
 import explorar from './explorar';
@@ -14,21 +13,17 @@ import sql from './sql/index';
 import privacidad from './privacidad';
 import categoria from './categoria';
 import clientes from './clientes';
-// import direccion from './direccion';
 import perfil from './perfil';
-// import chat from './chat';
-// import ayuda from './ayuda';
 
 import rol from './rol';
 import _public from "./public"
 import dm_categorias from './dm_categorias';
 import dm_productos from './dm_productos';
+import dm_usuarios from './dm_usuarios';
 export default SPage.combinePages("/", {
-  // "": test,
   "": carga,
   "root": root,
   ...login,
-  // mapa,
   ...registro,
   "explorar": explorar,
   "favoritos": favoritos,
@@ -40,13 +35,10 @@ export default SPage.combinePages("/", {
   "categoria": categoria,
 
   ...clientes,
-  // ...direccion,
   ...perfil,
   ...rol,
   ..._public,
   ...dm_categorias,
-  ...dm_productos
-  // ...chat,
-  // ...ayuda,
-  // ...ayuda
+  ...dm_productos,
+  ...dm_usuarios
 });
