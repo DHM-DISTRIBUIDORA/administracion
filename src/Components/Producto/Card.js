2be1dc4294c877bf1fedf8b70346c45cebde5038
Licethd <liceth.dfe@gmail.com>
2023-07-11 01:48:10 -0400

    botones productos

--- src/Components/Producto/Card.js
+++ src/Components/Producto/Card.js
@@ -11,6 +11,7 @@ class index extends Component<ProductoCardPropsType> {
     constructor(props) {
         super(props);
         this.state = {
+            count: 0,
         };
     }
 
@@ -79,15 +80,25 @@ class index extends Component<ProductoCardPropsType> {
                     <SHr />
                     <SView row>
                         <SView col={"xs-7"} row center>
-                            <SView center width={45} height={45} style={{ borderRadius: 17, borderColor: "#E2E2E2", borderWidth: 1 }}>
+                            <SView center width={45} height={45} style={{ borderRadius: 17, borderColor: "#E2E2E2", borderWidth: 1 }}
+                            onPress={() => {
+                                if (this.state.count <= 0) return;
+                                this.setState({ count: this.state.count - 1 });
+                            }} 
+                            >
                                 <SIcon name='Menos' height={4} />
                             </SView>
                             <SView row  >
                                 <SView width={10} />
-                                <SText fontSize={16}>0</SText>
+                                <SText fontSize={16}>{this.state?.count ? this.state.count : 0}</SText>
                                 <SView width={10} />
                             </SView>
-                            <SView center width={45} height={45} style={{ borderRadius: 17, borderColor: "#E2E2E2", borderWidth: 1 }}>
+                            <SView center width={45} height={45} style={{ borderRadius: 17, borderColor: "#E2E2E2", borderWidth: 1 }}
+                            onPress={() => {
+                                if (this.state.count >= Stock) return;
+                                this.setState({count: this.state.count + 1})
+                            }} 
+                            >
                                 <SIcon name='Mas' height={18} />
                             </SView>
                         </SView>
