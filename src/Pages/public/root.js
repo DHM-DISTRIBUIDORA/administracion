5a37e79a03cd5f8a2f48e54a48b78baa9941e2b2
rickypazd <rickypazd@hotmail.com>
2023-07-26 02:02:37 -0400

    Update root.js

--- src/Pages/public/root.js
+++ src/Pages/public/root.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ScrollView } from 'react-native'
+import { FlatList, ScrollView } from 'react-native'
 import { SHr, SPage, SText, SView, SLoad, STheme, SImage, SIcon, SNavigation, SList, SMath } from 'servisofts-component';
 import { Banner, BottomNavigator, Container, TopBar, } from '../../Components';
 import Model from '../../Model';
+const FotoPerfil = require('../../Assets/img/foto.png')
 class index extends Component {
 
     constructor(props) {
@@ -13,42 +14,17 @@ class index extends Component {
         };
     }
 
-    render_mas_vendidos() {
-        // var productos = Model.productos.Action.getAll();
-        // if (!productos) return <SLoad />
-        var productos = Model.tbprd.Action.getAllSimple();
-        if (!productos) return <SLoad />
-        return <SView col={"xs-12"} height={195}>
-            <SList
-                horizontal
-                initSpace={8}
-                data={productos}
-                render={(data) => {
-                    return <SView card width={318} height={150} style={{
-                        overflow: "hidden"
-                    }}>
-                        <SText>{data}</SText>
-                        {/* <SImage src={SSocket.api.root + "productos/" + data.key} style={{
-                            resizeMode: "cover"
-                        }} /> */}
-                    </SView>
-                }}
-            />
-            {/* </SScrollView2> */}
-        </SView>
-    }
 
     renderItem(obj) {
-        return <SView width={170} card
+        return <SView width={170} height={280} card
             style={{
-                padding: 10,
+                padding: 9,
                 borderRadius: 18,
                 borderWidth: 1,
-                borderColor: "#E2E2E2",
-
+                borderColor: STheme.color.card,
             }}>
-            <SView col={"xs-12"} row height={145}>
-                <SImage src={require('../../Assets/img/foto.png')} style={{ resizeMode: "contain" }} />
+            <SView col={"xs-12"} height={145} card>
+                <SImage src={FotoPerfil} style={{ resizeMode: "contain" }} />
             </SView>
             <SHr />
             <SText fontSize={16}>{obj.prdnom}</SText>
@@ -59,7 +35,7 @@ class index extends Component {
                 <SView col={"xs-8"}>
                     <SText fontSize={18} bold>Bs. {SMath.formatMoney(obj.prdpoficial)}</SText>
                 </SView>
-                <SView col={"xs-4"} flex style={{ alignItems: "flex-end" }}
+                <SView col={"xs-4"} style={{ alignItems: "flex-end" }}
                     onPress={() => {
                         const data = {
                             [obj.idprd]: { cantidad: 1, data: obj },
@@ -71,43 +47,41 @@ class index extends Component {
                     }}
                 >
                     {/* <SIcon name={(this.state.btn == 0) ? 'BtnMas' : 'Check2'} height={45} /> */}
-                    <SIcon name='BtnMas' height={45} />
+                    <SIcon name='BtnMas' height={30} />
                 </SView>
             </SView>
         </SView>
     }
 
+
+    publicidadItem({ src }) {
+        return <SView width={140} height={140} padding={4}>
+            <SImage enablePreview src={src} style={{ resizeMode: "contain", borderRadius: 8 }} />
+        </SView>
+    }
     publicidadBanners() {
-        return <ScrollView
-            horizontal
-            contentContainerStyle={{
-                width: null,
-            }}>
-            <SView width={8} />
-            <SView width={250} height={250}>
-                <SImage src={require('../../Assets/img/banner2.jpg')} style={{ resizeMode: "contain", borderRadius: 8 }} />
-            </SView>
-            <SView width={8} />
-            <SView width={250} height={250}>
-                <SImage src={require('../../Assets/img/banner3.jpg')} style={{ resizeMode: "contain", borderRadius: 8 }} />
-            </SView>
-            <SView width={8} />
-            <SView width={250} height={250}>
-                <SImage src={require('../../Assets/img/banner1.jpg')} style={{ resizeMode: "contain", borderRadius: 8 }} />
-            </SView>
-            <SView width={8} />
-            <SView width={250} height={250}>
-                <SImage src={require('../../Assets/img/banner4.jpg')} style={{ resizeMode: "contain", borderRadius: 8 }} />
-            </SView>
-            <SView width={8} />
-            <SView width={250} height={250}>
-                <SImage src={require('../../Assets/img/banner5.jpg')} style={{ resizeMode: "contain", borderRadius: 8 }} />
-            </SView>
-            <SView width={8} />
-            <SView width={250} height={250}>
-                <SImage src={require('../../Assets/img/banner6.jpg')} style={{ resizeMode: "contain", borderRadius: 8 }} />
-            </SView>
-        </ScrollView>
+        return <FlatList horizontal data={[
+            { src: require('../../Assets/img/banner1.jpg') },
+            { src: require('../../Assets/img/banner2.jpg') },
+            { src: require('../../Assets/img/banner3.jpg') },
+            { src: require('../../Assets/img/banner4.jpg') },
+            { src: require('../../Assets/img/banner5.jpg') },
+            { src: require('../../Assets/img/banner6.jpg') },
+            { src: require('../../Assets/img/banner1.jpg') },
+            { src: require('../../Assets/img/banner2.jpg') },
+            { src: require('../../Assets/img/banner3.jpg') },
+            { src: require('../../Assets/img/banner4.jpg') },
+            { src: require('../../Assets/img/banner5.jpg') },
+            { src: require('../../Assets/img/banner6.jpg') },
+            { src: require('../../Assets/img/banner1.jpg') },
+            { src: require('../../Assets/img/banner2.jpg') },
+            { src: require('../../Assets/img/banner3.jpg') },
+            { src: require('../../Assets/img/banner4.jpg') },
+            { src: require('../../Assets/img/banner5.jpg') },
+            { src: require('../../Assets/img/banner6.jpg') },
+        ]}
+            renderItem={row => this.publicidadItem(row.item)}
+        />
     }
 
     render() {
@@ -137,8 +111,6 @@ class index extends Component {
                 </SView>
 
             </Container>
-
-            {/* {this.render_mas_vendidos()} */}
             <SView col={"xs-12"}>
                 <ScrollView
                     horizontal
@@ -157,7 +129,6 @@ class index extends Component {
             <SHr height={25} />
             <SView col={"xs-12"}>
                 {this.publicidadBanners()}
-
             </SView>
             <SHr height={30} />
         </SPage >
