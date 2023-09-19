41fa42d7670888918e03d14684089258c2acd17e
rickypazd <rickypazd@hotmail.com>
2023-09-18 22:20:04 -0400

    a

--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -44,7 +44,7 @@ import test2 from "./test2"
 import test from "./test"
 
 // import storage from "./storage"
-// import { Example } from "servisofts-charts"
+import { Example } from "servisofts-charts"
 
 // export default SPage.combinePages("/", {
 //   "":storage,
@@ -92,5 +92,5 @@ export default SPage.combinePages("/", {
   // test2,
   // test3,
   // test,
-  // "chart": Example
+  "chart": Example
 });
