ebcdf924cb16e38f9c226f682b8d7affc52a89d5
Licethd <liceth.dfe@gmail.com>
2023-09-27 01:45:27 -0400

    f5

--- src/Pages/tbemp/profile/pedidos.js
+++ src/Pages/tbemp/profile/pedidos.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { SDate, SHr, SList, SMath, SNavigation, SPage, SText, SView } from 'servisofts-component';
+import { SDate, SHr, SList, SLoad, SMath, SNavigation, SPage, SText, SView } from 'servisofts-component';
 import SSocket from 'servisofts-socket'
 import { Container } from '../../../Components';
+import Model from '../../../Model';
 class pedidos extends Component {
     constructor(props) {
         super(props);
@@ -33,20 +34,30 @@ class pedidos extends Component {
         })
         return <SView col={"xs-12"} card padding={8} onPress={() => {
             SNavigation.navigate("/dm_cabfac/recibo", { pk: obj.idven })
-        }}>
-            <SView col={"xs-12"} flex row>
+        }} row>
+            <SView col={"xs-12"} row>
                 <SText>{obj.idven}</SText>
                 <SView flex />
                 <SText>{(obj.vfec + "").substring(0, 10)}</SText>
             </SView>
-            <SText>{obj.clicod}  - {obj.nombrecliente}</SText>
-            <SText>{obj.razonsocial}</SText>
-            <SText bold># {cantidadProductos}</SText>
-            <SText bold>Bs. {SMath.formatMoney(total)}</SText>
+            <SView col={"xs-12"} row>
+                <SView col={"xs-3"} row>
+
+                </SView>
+                <SView col={"xs-9"} flex row>
+                    <SText>{obj.clicod}  - {obj.nombrecliente}</SText>
+                    <SText>{obj.razonsocial}</SText>
+                    <SText bold># {cantidadProductos}</SText>
+                    <SText bold>Bs. {SMath.formatMoney(total)}</SText>
+                </SView>
+
+            </SView>
+
 
         </SView>
     }
     render() {
+        if (!this.state.data) return <SLoad />
         return (
             <SPage title={'Pedidos'} >
                 <Container >
@@ -55,7 +66,7 @@ class pedidos extends Component {
                         <SView col={"xs-12"} center>
                             <SText fontSize={20} bold >Pedidos entre: </SText>
                             <SText fontSize={20} bold >{this.fecha_inicio} , {this.fecha_fin}</SText>
-                        </SView> : null }
+                        </SView> : null}
 
                     <SHr height={20} />
                     <SList
