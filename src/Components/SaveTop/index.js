be980306a7dfa67a1610a679437e3e32322183a9
Liceth Flores <72888760+Licethd@users.noreply.github.com>
2023-10-25 02:18:02 -0400

    cambios

---
+++ src/Components/SaveTop/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Animated } from 'react-native';
+import { SView, SImage, SNavigation, STheme, SIcon, SText, SScrollView2, SThread } from 'servisofts-component';
+import { connect } from 'react-redux';
+import DataBaseContainer from '../../DataBase/DataBaseContainer';
+
+class index extends React.Component {
+	
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			
+		};
+		
+	}
+	
+
+	render() {
+	
+		return (
+			<>
+				<SView card style={{
+					position: "absolute",
+					width: 25,
+					height: 25,
+					right: 70,
+				}}
+					activeOpacity={1}
+					onPress={() => {
+						DataBaseContainer.sync();
+					}} center>
+						<SIcon name={"Isave"} fill={STheme.color.primary} height={17} width={17} />
+				</SView>
+			
+			</>
+		);
+	}
+}
+
+const initStates = (state) => {
+	return { state }
+};
+export default connect(initStates)(index);
