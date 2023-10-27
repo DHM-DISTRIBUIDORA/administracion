a0fe2972600eaad5e2939cb150ded2825653a3d9
rickypazd <rickypazd@hotmail.com>
2023-10-27 18:48:50 -0400

    ds

--- src/Pages/test.js
+++ src/Pages/test.js
@@ -11,7 +11,8 @@ export default class test extends Component {
     componentDidMount() {
 
         console.log("ENTRO AQUI EJEUCATO")
-        this.init("04759652-b279-40ea-817d-dbfbfc39ffa5","2023-8-28");
+        // this.init("04759652-b279-40ea-817d-dbfbfc39ffa5","2023-8-28");
+        this.init("27c58652-991d-435a-9207-e6bd5db16896","2023-10-27");
         //this.init("c6960922-79a8-4d22-8774-3336a2718f41", "2023-9-19");
         //this.init("7929777a-8cea-4c34-aec8-a22bb7439fac","2023-9-27");
 
