93aab7e39e0755ebdd3f169bd4c9daf887dd709d
rickypazd <rickypazd@hotmail.com>
2023-11-01 23:54:07 -0400

    as

--- src/Pages/test.js
+++ src/Pages/test.js
@@ -1,32 +1,22 @@
 import { Text, View } from 'react-native'
 import React, { Component, useState } from 'react'
-
-import DomSelector from 'react-native-dom-parser';
 import { SMapView, SPage, SText } from 'servisofts-component';
+
+import { check,request, PERMISSIONS, RESULTS } from 'react-native-permissions';
+
 export default class test extends Component {
     state = {
-        url: "https://repo.dhm.servisofts.com/dhm/gpx"
     };
 
+    componentDidMount() {
+        request(PERMISSIONS.ANDROID.CAMERA).then((result) => {
+            // …
+        });
+    }
     render() {
-        return (
-            <SPage disableScroll>
-                <SMapView initialRegion={{
-                    latitude: -17.783799,
-                    longitude: -63.180,
-                    latitudeDelta: 0.1,
-                    longitudeDelta: 0.1
-                }}
-                    onPress={(e) => {
-                        console.log(e);
-                        if (this.marker) {
-                            this.marker.setCoordinate(e.coordinate)
-                        }
-                    }}
-                >
-                    <SMapView.SMarker ref={ref => this.marker = ref} latitude={-17.78} longitude={-63.180} />
-                </SMapView>
 
+        return (
+            <SPage title={"Test"}>
             </SPage>
         )
 
