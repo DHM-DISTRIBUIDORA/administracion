8144a25578a58a03f69be31f4f659acab5e39f6a
Liceth Flores <72888760+Licethd@users.noreply.github.com>
2023-11-14 20:32:16 -0400

    f5

--- src/Pages/blanco/index.ts
+++ src/Pages/blanco/index.ts
@@ -3,6 +3,7 @@ import { SPage } from 'servisofts-component';
 import root from './root';
 import BuggyCounter from './BuggyCounter';
 import ErrorBoundary from './ErrorBoundary';
+import blanco2 from './blanco2';
 
 export const Parent = {
     name: "blanco",
@@ -12,4 +13,5 @@ export default SPage.combinePages(Parent.name, {
     "": root,
     "BuggyCounter": BuggyCounter,
     "ErrorBoundary": ErrorBoundary,
+    "blanco2": blanco2,
 });
