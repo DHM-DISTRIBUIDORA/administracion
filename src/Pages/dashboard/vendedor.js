d3f92bddee5fc81668aa587217182eb85125c093
Liceth Flores <72888760+Licethd@users.noreply.github.com>
2023-11-21 01:53:46 -0400

    now

--- src/Pages/dashboard/vendedor.js
+++ src/Pages/dashboard/vendedor.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import SSocket from 'servisofts-socket'
-import { SLoad, SMath, SPage, STable2, SText, SView } from 'servisofts-component'
+import { SHr, SList, SLoad, SMath, SPage, STable2, SText, SView } from 'servisofts-component'
+import { Container, Dashboard } from '../../Components';
+import DataBase from '../../DataBase';
 export default class index extends Component {
     state = {
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getData();
+        DataBase.usuario.all().then(e => {
+            this.setState({ usuarios: e })
+
+        })
     }
     getData() {
         const request = {
@@ -15,7 +21,7 @@ export default class index extends Component {
         }
         this.setState({ loading: true })
         SSocket.sendHttpAsync(SSocket.api.root + "api", request).then(e => {
-            console.log(e);
+            // console.log(e);
             this.setState({ data: e.data, loading: false })
         }).catch(e => {
             this.setState({ loading: false, error: e?.error })
@@ -24,11 +30,26 @@ export default class index extends Component {
     }
 
     render() {
+        // const data = this.state?.data;
+        if (!this.state?.data) return <SLoad />
         return (
-            <SPage title="Pedidos por vendedores" disableScroll>
-                <SView flex>
-                    <SText>{JSON.stringify(this.state)}</SText>
-                </SView>
+            <SPage title="Pedidos por vendedores" >
+                <Container>
+                    {/* <SText>{JSON.stringify(this.state)}</SText> */}
+                    <SList
+                        data={this.state.data}
+                        limit={20}
+                        buscador
+                        // filter={(a) => a.nivel == 1}
+                        // order={[{ key: "idven", order: "desc" }]}
+                        render={(obj) => {
+                            var obj2 = this.state.usuarios.filter(e => e.idvendedor == obj.idemp)
+                            obj.key = obj2[0].key
+                            return <Dashboard.Card data={obj}  />
+                        }}
+                    />
+                    <SHr height={20} />
+                </Container>
             </SPage>
         )
     }
