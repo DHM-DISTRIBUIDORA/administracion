59df6fa52b8dbd59a7634386db922358e3fe0f08
rickypazd <rickypazd@hotmail.com>
2023-11-22 01:19:51 -0400

    as

--- src/Pages/dashboard/vendedor.js
+++ src/Pages/dashboard/vendedor.js
@@ -3,18 +3,19 @@ import SSocket from 'servisofts-socket'
 import { SHr, SList, SLoad, SMath, SPage, STable2, SText, STheme, SView } from 'servisofts-component'
 import { Container, Dashboard } from '../../Components';
 import DataBase from '../../DataBase';
+import { SelectFecha } from '../../Components/Fechas';
 export default class index extends Component {
     state = {
     }
 
     componentDidMount() {
-        this.getData();
 
     }
-    getData() {
+    getData({ fecha }) {
         const request = {
             component: "dhm",
             type: "dashboardVendedor",
+            fecha: fecha
         }
         this.setState({ loading: true })
         SSocket.sendHttpAsync(SSocket.api.root + "api", request).then(async e => {
@@ -33,27 +34,34 @@ export default class index extends Component {
     }
 
 
-    render() {
+    renderData() {
         // const data = this.state?.data;
         if (this.state?.error) return <SText color={STheme.color.danger}>{JSON.stringify(this.state.error)}</SText>
         if (!this.state?.data) return <SLoad />
+        return <SList
+            data={this.state.data}
+            limit={20}
+            buscador
+            // filter={(a) => a.nivel == 1}
+            // order={[{ key: "idven", order: "desc" }]}
+            render={(obj) => {
+                // var obj2 = this.state.usuarios.filter(e => e.idvendedor == obj.idemp)
+                // obj.key = obj2[0].key
+                return <Dashboard.Card data={obj} />
+            }}
+        />
+    }
+    render() {
+
         return (
             <SPage title="Pedidos por vendedores" >
                 <Container>
-                    {/* <SText>{JSON.stringify(this.state)}</SText> */}
-                    <SList
-                        data={this.state.data}
-                        limit={20}
-                        buscador
-                        // filter={(a) => a.nivel == 1}
-                        // order={[{ key: "idven", order: "desc" }]}
-                        render={(obj) => {
-                            // var obj2 = this.state.usuarios.filter(e => e.idvendedor == obj.idemp)
-                            // obj.key = obj2[0].key
-                            return <Dashboard.Card data={obj} />
-                        }}
-                    />
-                    <SHr height={20} />
+                    <SelectFecha onChange={(e) => {
+                        this.setState({ data: null })
+                        this.getData({ fecha: e.fecha });
+                    }} />
+                    {this.renderData()}
+                    <SHr h={20} />
                 </Container>
             </SPage>
         )
