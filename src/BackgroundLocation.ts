a7a2ab7bce6f31fecd5c57d36257a8add56236b0
rickypazd <rickypazd@hotmail.com>
2023-11-28 13:58:25 -0400

    commit

--- src/BackgroundLocation.ts
+++ src/BackgroundLocation.ts
@@ -1,12 +1,12 @@
-import { SBLocation } from 'servisofts-background-location';
+// import { SBLocation } from 'servisofts-background-location';
 import Model from './Model';
 import { Platform } from 'react-native';
 export default () => {
     // console.log("******* ENTRO A BackgroundLocation *********")
     // SBLocation.stop();
-    if (Platform.OS == "web") return;
-    SBLocation.initEmitter((data) => {
-        Model.background_location.Action.onChange(data.data, data.type);
-        return true;
-    })
+    // if (Platform.OS == "web") return;
+    // SBLocation.initEmitter((data) => {
+    //     Model.background_location.Action.onChange(data.data, data.type);
+    //     return true;
+    // })
 }
